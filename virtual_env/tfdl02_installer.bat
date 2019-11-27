bcc12bc6bec6046f5c49f14bfb14cb6d624803ac
eufmike <sc2.shih@gmail.com>
2019-11-27 11:51:37 -0600

    minor edit

--- virtual_env/tfdl02_installer.bat
+++ virtual_env/tfdl02_installer.bat
@@ -4,6 +4,5 @@ CALL conda.bat activate tfdl02
 CALL conda.bat install -c conda-forge numpy scipy matplotlib imutils pandas tdqm -y
 CALL conda.bat install -c conda-forge jupyter -y
 CALL conda.bat install -c conda-forge pillow scikit-image scikit-learn -y
-pip install opencv-python==3.4.2.16
 CALL conda.bat install -c anaconda tensorflow-gpu -y
-
+pip install opencv-python==3.4.2.16
