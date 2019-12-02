3deb50e95036669d5b084e6cea15cc3fffc4c162
Michael Shih <sc2.shih@gmail.com>
2019-12-02 12:36:52 -0600

    fix env installer: tqdm

--- virtual_env/tfdl02_installer.bat
+++ virtual_env/tfdl02_installer.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 CALL conda.bat create -n tfdl02 python=3.6 -y
 CALL conda.bat activate tfdl02
-CALL conda.bat install -c conda-forge numpy scipy matplotlib imutils pandas tdqm -y
+CALL conda.bat install -c conda-forge numpy scipy matplotlib imutils pandas tqdm -y
 CALL conda.bat install -c conda-forge jupyter -y
 CALL conda.bat install -c conda-forge pillow scikit-image scikit-learn -y
 CALL conda.bat install -c anaconda tensorflow-gpu -y
