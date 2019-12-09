a21950fc1c9e8ae42c76adffe0ec94a33cd1766f
eufmike <sc2.shih@gmail.com>
2019-12-09 16:41:45 -0600

    add seg

--- core/train_predict.py
+++ core/train_predict.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageTk
 from tqdm.notebook import trange
 from core.imageprep import create_crop_idx, crop_to_patch, construct_from_patch
 
+import matplotlib.pyplot as plt
 
 def stack_predict(input_imgpath, 
                   output_imgpath, 
@@ -47,3 +48,57 @@ def stack_predict(input_imgpath,
         # save image
         outputimg_T_pillow = Image.fromarray(outputimg_T)
         outputimg_T_pillow.save(os.path.join(output_imgpath, os.path.basename(inputimg)))
+
+def stack_predict_v2(input_imgpath, 
+                  output_imgpath, 
+                  # cropidx, 
+                  model, 
+                  rescale = None,
+                  # patch_size = (256, 256), 
+                  predict_threshold = 0.5):
+    
+    # IMG_HEIGHT = patch_size[0]
+    # IMG_WIDTH = patch_size[1]
+                  
+    for idx in trange(len(input_imgpath)):
+        
+        inputimg = input_imgpath[idx]
+        
+        # load image
+        img_tmp = imread(inputimg)
+        
+        plt.imshow(img_tmp)
+        
+        # process rescale
+        if rescale is not None:  
+            img_tmp = img_tmp * rescale 
+        
+        img_tmp = img_tmp.reshape(1, img_tmp.shape[0], img_tmp.shape[1], 1)
+        
+        print(img_tmp.shape)
+        
+        '''
+        # crop the image
+        outputimg_tmp = crop_to_patch(img_tmp, cropidx, (IMG_HEIGHT, IMG_WIDTH))
+        outputimg_tmp_re = np.reshape(outputimg_tmp, (outputimg_tmp.shape[0], 
+                                                      outputimg_tmp.shape[1], 
+                                                      outputimg_tmp.shape[2], 1))
+        
+        # push the crop images into the model
+        img_predict_stack = model.predict(outputimg_tmp_re, batch_size = 16, verbose = 1)
+        
+        outputimg = construct_from_patch(img_predict_stack, 
+                                         cropidx, 
+                                         target_size = (img_tmp.shape[0], img_tmp.shape[1]))
+        '''
+        
+        img_predict = model.predict(img_tmp, batch_size = 16, verbose = 1)
+        
+        # threshold the image
+        outputimg_T = img_predict > predict_threshold
+        
+        # save image
+        outputimg_T_pillow = Image.fromarray(outputimg_T)
+        outputimg_T_pillow.save(os.path.join(output_imgpath, os.path.basename(inputimg)))
+        
+        break
