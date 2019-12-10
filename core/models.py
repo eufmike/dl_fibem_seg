361b0878f446f4267a77cee3542435c3c8209a72
eufmike <sc2.shih@gmail.com>
2019-12-10 17:01:30 -0600

    tensorboard preparing for custom metrics

--- core/models.py
+++ core/models.py
@@ -8,7 +8,114 @@ from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.optimizers import Adam, Adadelta
 from tensorflow.keras import backend as K
 
-def UNet(shape, nClasses=1, loss="binary_crossentropy", lr=1e-5, metrics=['accuracy']):
+def UNet(shape, 
+         nClasses=1, 
+         loss="binary_crossentropy", 
+         lr=1e-5, 
+         metrics=['accuracy']):
+    
+    IMG_HEIGHT = shape[0]
+    IMG_WIDTH = shape[1]
+    IMG_CHANNELS = nClasses
+
+    # Build U-Net model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+
+
+    pool1 = MaxPool2D(pool_size=(2, 2))(conv1)
+
+
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+
+    pool2 = MaxPool2D(pool_size=(2, 2))(conv2)
+
+
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+
+    pool3 = MaxPool2D(pool_size=(2, 2))(conv3)
+
+
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPool2D(pool_size=(2, 2))(drop4)
+
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+
+    up6 = Conv2D(512, 2, 
+                 activation = 'relu', 
+                 padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+    
+    merge6 = concatenate([drop4,up6])
+
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2D(256, 2, 
+                 activation = 'relu', 
+                 padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+    
+    merge7 = concatenate([conv3, up7])
+
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv2D(128, 2, 
+                 activation = 'relu', 
+                 padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+    
+    merge8 = concatenate([conv2, up8])
+
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+    up9 = Conv2D(64, 2, 
+                 activation = 'relu', 
+                 padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+    
+    merge9 = concatenate([conv1, up9])
+
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+
+    output_shape = Model(inputs , conv9 ).output_shape
+
+    output = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+    
+    model = Model(inputs, output)
+    model.compile(loss=loss, optimizer = Adam(lr = lr) , metrics=metrics)
+    model.summary()
+
+    return model
+
+def iou_coef(y_true, y_pred, smooth=1):
+    intersection = K.sum(K.abs(y_true * y_pred), axis=[1,2,3])
+    union = K.sum(y_true,[1,2,3])+K.sum(y_pred,[1,2,3])-intersection
+    iou = K.mean((intersection + smooth) / (union + smooth), axis=0)
+    return iou
+
+def dice_coef(y_true, y_pred, smooth=1):
+    intersection = K.sum(y_true * y_pred, axis=[1,2,3])
+    union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3])
+    dice = K.mean((2. * intersection + smooth)/(union + smooth), axis=0)
+    return dice
+
+
+############################ Retired Code ##################################
+
+
+
+def UNet_old(shape, nClasses=1, loss="binary_crossentropy", lr=1e-5, metrics=['accuracy']):
     
     IMG_HEIGHT = shape[0]
     IMG_WIDTH = shape[1]
